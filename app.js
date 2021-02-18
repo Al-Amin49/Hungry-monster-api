c121d8eca8998d3db1ba22f082e8bbb6fba05bda
Al-Amin49 <alamin496832@gmail.com>
2021-02-19 03:00:45 +0600

    have to do more thing

---
+++ app.js
@@ -0,0 +1,20 @@
+const searchBtn = document.getElementById("search-btn").addEventListener('click', () =>  { 
+    const searchInput = document.getElementById('search-input').Value;
+    console.log("amake click korse");
+
+
+
+})
+
+
+
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+    .then(res=> res.json())
+    .then(data=>{ 
+
+    })
+    const 
+
+
+
+  
